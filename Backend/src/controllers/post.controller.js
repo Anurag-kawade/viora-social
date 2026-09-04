048b07df2e99efba1c21c81f8aebda1268050abf
Anurag-kawade <anurag.workzone@gmail.com>
2026-09-04 15:29:46 +0530

    Created post feature for users

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,20 +1,46 @@
 const postModel = require("../models/post.model");
 const imageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const imagekit = new imageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function createPostController(req, res) {
-  console.log(req.body, req.file);
+  const token = req.cookies.token;
+
+  if (!token) {
+    res.status(401).json({
+      message: "Token not provided , Unauthorized access",
+    });
+  }
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).json({
+      message: "Unauthorized access",
+    });
+  }
 
   const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "Test",
+    folder: "Viora",
   });
 
-  res.send(file)
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: file.url,
+    user: decoded.id,
+  });
+
+  res.status(201).json({
+    message: "Post created successfully.",
+    post,
+  });
 }
 
 module.exports = {
